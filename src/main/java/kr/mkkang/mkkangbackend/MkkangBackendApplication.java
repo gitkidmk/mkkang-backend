80c325302925a46a65481a5901fecfa4d796c14b
gitkidmk <lovekangmk@gmail.com>
2024-07-28 00:07:40 +0900

    [feat] first commit

---
+++ src/main/java/kr/mkkang/mkkangbackend/MkkangBackendApplication.java
@@ -0,0 +1,13 @@
+package kr.mkkang.mkkangbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MkkangBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MkkangBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/mkkang/mkkangbackend/MkkangBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.mkkang.mkkangbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MkkangBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
